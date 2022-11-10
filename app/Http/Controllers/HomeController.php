cf97e8b0bf880a6ea320554de333100c85212ecf
Dangar4ik <treninomok1@gmail.com>
2022-11-10 14:26:20 +0300

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    public function index(): View
+    {
+        return view('home', [
+            'nameone' => 'Фильм первый',
+            'descriptionone' => 'Описание Первое',
+            'nametwo' => 'Фильм второй',
+            'descriptiontwo' => 'Описание Второе',
+            'namethree' => 'Фильм третий',
+            'descriptionthree' => 'Описание Третье',
+            'namefour' => 'Фильм четвертый',
+            'descriptionfour' => 'Описание Четвертое'
+        ]);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+<?php
+?>
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Хоумлендер сайт</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body class="antialiased">
+<img src="{{asset('image/pumpkin.webp')}}">
+<a>Картинка- </a>
+<div class="container">
+
+<div class="style" >
+    <a href="" class="underline">
+        {{ $nameone }}
+    </a>
+<br>{{ $descriptionone }}
+</div>
+    <div class="style">
+        <a href="" class="underline">
+            {{ $nametwo }}
+        </a>
+<br>{{ $descriptiontwo }}
+    </div>
+    <div class="style">
+        <a href="" class="underline">
+            {{ $namethree}}
+        </a>
+<br> {{ $descriptionthree }}
+    </div>
+    <div class="style">
+        <a href="" class="underline">
+            {{ $namefour }}
+        </a>
+<br>{{ $descriptionfour }}
+    </div>
+</div>
+</body>
+</html>
